33a38ce80e59dc7324cd641bb23251fcbfbe8e2a
Saifullah Chowdhury <saifullahchy0177@gmail.com>
2020-11-20 18:02:26 +0600

    still have image problem. need help from RObin

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -9,7 +9,7 @@ export const homeObjOne = {
     description: 'We provide A to Z solution for your business. Web-development, Android Application, POS System, Digital Marketing and what not? We providing everything you need',
     buttonLabel: 'Get Started',
     imgStart: false,
-    img: require('../../images/svg-1.svg'),
+    img:"images/svg-1.svg",
     alt:'Svg 1',
     dark: true,
     primary: true,
